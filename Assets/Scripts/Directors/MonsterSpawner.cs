d800bd55e5e997c6f458c4abefc233ff45267ac6
juan.rod707 <juan.rod707@gmail.com>
2022-01-30 13:45:34 -0300

    added champions

--- Assets/Scripts/Directors/MonsterSpawner.cs
+++ Assets/Scripts/Directors/MonsterSpawner.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts.Actors.Monsters;
 using Assets.Scripts.Common;
 using Assets.Scripts.Persistence;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Assets.Scripts.Directors
 {
     public class MonsterSpawner : MonoBehaviour
     {
-        [SerializeField] Monster[] monsterPrefabs;
+        [SerializeField] MonsterWeightedList monsterPrefabs;
 
         [SerializeField] int baseAmount;
         [SerializeField] float radius;
@@ -28,13 +30,38 @@ namespace Assets.Scripts.Directors
 
             foreach (var _ in Enumerable.Range(0, amountToSpawn))
             {
-                var prefab = monsterPrefabs.PickOne();
+                var prefab = monsterPrefabs.TakeOne();
                 var monster = Instantiate(prefab, transform);
                 var spawnPos = Random.insideUnitCircle * radius;
                 monster.transform.position = new Vector3(spawnPos.x, 0, spawnPos.y);
                 monsters.Add(monster);
             }
 
+        }
+
+        [Serializable]
+        public class MonsterWeightPair
+        {
+            public Monster Monster;
+            public int Weight;
+        }
+
+        [Serializable]
+        public class MonsterWeightedList
+        {
+            [SerializeField] MonsterWeightPair[] materialWeights;
+
+            public Monster TakeOne()
+            {
+                var flatList = new List<Monster>();
+                foreach (var mw in materialWeights)
+                {
+                    foreach (var _ in Enumerable.Range(0, mw.Weight))
+                        flatList.Add(mw.Monster);
+                }
+
+                return flatList.PickOne();
+            }
         }
     }
 }
